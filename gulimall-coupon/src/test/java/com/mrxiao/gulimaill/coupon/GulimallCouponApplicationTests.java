01ccbead415c6a4c86d6df18d9e18446680d258b
mrxiao_global <mrxiao_global@163.com>
2020-05-12 18:48:59 +0800

    项目初始化

---
+++ gulimall-coupon/src/test/java/com/mrxiao/gulimaill/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrxiao.gulimaill.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
